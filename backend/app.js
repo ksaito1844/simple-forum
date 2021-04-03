e68cadc4606ca33520eea8dd556f29096507498c
Dustin Simensen <dustinsimensen@gmail.com>
2021-04-02 17:12:11 -0700

    one more try, maybe

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 app.get('/*', (req,res) =>{
-  res.sendFile(path.join(__dirname + 'backend/build/index.html'));
+  res.use(express.static('build'))
 });
 
 app.use(middleware.unknownEndpoint)
