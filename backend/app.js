0aea8aa9cc1617a799ff607507b180ebc0078053
Dustin Simensen <dustinsimensen@gmail.com>
2021-04-02 17:03:06 -0700

    maybe the path is wrong

--- backend/app.js
+++ backend/app.js
@@ -36,7 +36,7 @@ if (process.env.NODE_ENV === 'test') {
   app.use('/api/testing', testingRouter)
 }
 app.get('/*', (req,res) =>{
-  res.sendFile(path.join(__dirname + '/build/index.html'));
+  res.sendFile(path.join(__dirname + '/backend/build/index.html'));
 });
 
 app.use(middleware.unknownEndpoint)
