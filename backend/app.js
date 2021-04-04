18af4912747f831a8b3decad42a92b996daf45e0
Dustin Simensen <dustinsimensen@gmail.com>
2021-04-03 19:17:42 -0700

    trying more catch all stuff

--- backend/app.js
+++ backend/app.js
@@ -19,9 +19,9 @@ mongoose.connect(config.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology
   .catch((error) => {
     logger.error('error connection to MongoDB:', error.message)
   })
-
+const staticServe = express.static('build');
 app.use(cors())
-app.use(express.static('build'))
+app.use(staticServe)
 app.use(express.json())
 app.use(middleware.requestLogger)
 app.use(middleware.tokenExtractor)
@@ -35,10 +35,7 @@ if (process.env.NODE_ENV === 'test') {
   const testingRouter = require('./controllers/testing')
   app.use('/api/testing', testingRouter)
 }
-app.get('/*', (req,res) =>{
-  res.use(express.static('build'))
-});
-
+app.use('/*', staticServe)
 app.use(middleware.unknownEndpoint)
 app.use(middleware.errorHandler)
 
